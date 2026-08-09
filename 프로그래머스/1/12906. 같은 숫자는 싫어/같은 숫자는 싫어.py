749e6f779834bbb043702407c9d38958104ca4d0
oheunhyuck <71165142+oheunhyuck@users.noreply.github.com>
2026-08-09 09:32:12 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 29.74 ms, Memory: 26.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = []
+    l=-1
+    for i in arr:
+        if(i==l):continue
+    
+        l=i
+        answer.append(i)           
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    
+    return answer
